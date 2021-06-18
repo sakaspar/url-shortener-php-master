6440156db815dd76b496574b7116e25041ca380e
sakaspar <travis@travis-ci.org>
2021-06-18 14:15:40 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+
+// SITE NAME
+
+if ( !defined('SITE_NAME') )
+{
+    define('SITE_NAME', "Make It Short!");
+}
+
+// URL LOCATION (Don't forget "/" at the end !)
+
+if ( !defined('BASE_URL') )
+{
+    define('BASE_URL', "shortmylinkbroda.rf.gd/");
+}
+
+// DATABASE CONFIGURATION
+
+if ( !defined('HOST_NAME') )
+{
+    define('HOST_NAME', "sql102.epizy.com");
+}
+
+if ( !defined('DB_NAME') )
+{
+    define('DB_NAME', "epiz_28900291_hamza");
+}
+
+if ( !defined('USER_NAME') )
+{
+    define('USER_NAME', "epiz_28900291");
+}
+
+if ( !defined('USER_PASSWORD') )
+{
+    define('USER_PASSWORD', "BAptjjgVeAyinwK");
+}
